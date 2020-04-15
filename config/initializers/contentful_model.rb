e9995d0aeec3e0edfba2ce6ae6c783bdd2071374
H-Asakawa <erimingo2718@yahoo.co.jp>
2020-04-15 19:38:22 +0900

    各属性値が返却できるか確認

--- config/initializers/contentful_model.rb
+++ config/initializers/contentful_model.rb
@@ -1,9 +1,9 @@
-# optionはこちらが読み込まれる
-ContentfulModel.configure do |config|
-  config.access_token         = ENV['CTF_CDA_ACCESS_TOKEN'] # Required
-  config.preview_access_token = ENV['CTF_CPA_ACCESS_TOKEN'] # Optional - required if you want to use the preview API
-  config.management_token     = ENV['CTF_CM_ACCESS_TOKEN'] # Optional - required if you want to update or create content
-  config.space                = ENV['CTF_SPACE_ID'] # Required
-  config.environment          = "master" # Optional - defaults to 'master'
-  config.default_locale       = "en-US" # Optional - defaults to 'en-US'
-end
+# # optionはこちらが読み込まれる
+# ContentfulModel.configure do |config|
+#   config.access_token         = ENV['CTF_CDA_ACCESS_TOKEN'] # Required
+#   config.preview_access_token = ENV['CTF_CPA_ACCESS_TOKEN'] # Optional - required if you want to use the preview API
+#   config.management_token     = ENV['CTF_CM_ACCESS_TOKEN'] # Optional - required if you want to update or create content
+#   config.space                = ENV['CTF_SPACE_ID'] # Required
+#   config.environment          = "master" # Optional - defaults to 'master'
+#   config.default_locale       = "en-US" # Optional - defaults to 'en-US'
+# end
